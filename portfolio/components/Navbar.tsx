96ab45a76fe0d3faedf843230c78716d04537848
Espenjansen <ejansen1201@gmail.com>
2023-05-22 09:14:52 +0200

    updates

--- portfolio/components/Navbar.tsx
+++ portfolio/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                 <Image src="/../public/img/E.png"
                 alt="/" 
                 width="100"  
-                height="50"
+                height="40"
                 />
                 <div>
                     <ul className='hidden md:flex'>
